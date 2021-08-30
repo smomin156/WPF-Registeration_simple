3bd491f7d439d9e1790467215383292a9f501fd0
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-31 00:25:52 +0530

    Add files via upload

---
+++ UserRegisteration/UserRegisteration/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserRegisteration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int chance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            chance = 3;
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            var name = txtName.Text;
+            var password = txtPassword.Password;
+            if (name == "admin" && password == "123")
+            {
+                MessageBox.Show("Login successful", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                RegisterationForm form = new RegisterationForm();
+                form.ShowDialog();
+            }
+            else if(chance==0)
+            {
+                MessageBox.Show("You have exhaused all attempts","Invalid",MessageBoxButton.OK,MessageBoxImage.Error);
+                this.Close();
+            }
+           else
+            {           
+                MessageBox.Show($"Wrong credentials You have {chance} attempts", "Invalid", MessageBoxButton.OK, MessageBoxImage.Error);
+                chance--;
+            }
+            
+        }
+    }
+}
---
+++ UserRegisteration/UserRegisteration/RegisterationForm.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace UserRegisteration
+{
+    /// <summary>
+    /// Interaction logic for RegisterationForm.xaml
+    /// </summary>
+    public partial class RegisterationForm : Window
+    {
+        public RegisterationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsername_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "This is required !!";
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+            string address = txtAddress.Text;
+            string country = txtCountry.Text;
+            string tel="No tel";
+            string mobile="No Mobile";
+            if (chkTel.IsChecked == true)
+            {
+                tel = txtTel.Text;
+            }
+            if (chkMob.IsChecked == true)
+            {
+                mobile = txtMobile.Text;
+            }
+            string gender = chkMale.IsChecked == true ? "Male" : "Female";
+            string marital = chkMarried.IsChecked == true ? "Married" : "single";
+            string qualify = "";
+
+            try
+            {
+                using(StreamWriter writer=new StreamWriter(@"C:\UserData\"+username+".txt"))
+                {
+                    writer.WriteLine("username : " + username);
+                    writer.WriteLine("password : " + password);
+                    writer.WriteLine("address : " + address);
+                    writer.WriteLine("country : " + country);
+                    writer.WriteLine("tel : " + tel);
+                    writer.WriteLine("mobile : " + mobile);
+                    writer.WriteLine("gender : " + gender);
+                    writer.WriteLine("marital status : " + marital);
+                    foreach (object item in listQualification.SelectedItems)
+                    {
+                        ListBoxItem li = (ListBoxItem)item;
+                        qualify += li.Content + " ";
+                    }
+                    writer.WriteLine("qualification : " + qualify);
+
+                }
+                MessageBox.Show("Data Saved Successesfully!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void chkTel_Checked(object sender, RoutedEventArgs e)
+        {
+            txtTel.Visibility = Visibility.Visible;
+        }
+
+        private void chkMob_Checked(object sender, RoutedEventArgs e)
+        {
+            txtMobile.Visibility = Visibility.Visible;
+        }
+
+        private void chkTel_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtTel.Visibility = Visibility.Hidden;
+        }
+
+        private void chkMob_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtMobile.Visibility = Visibility.Hidden;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            string[] arr = Directory.GetFiles(@"C:\UserData");
+            foreach (string n in arr)
+            {
+                listUser.Items.Add(n);
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            listUser.Items.Clear();
+        }
+
+        private void listUser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            object item = listUser.SelectedItem;
+            string path = item.ToString();
+            string line = "";
+            try
+            {
+                using(StreamReader reader=new StreamReader(path))
+                {
+                    line = reader.ReadToEnd();
+                    txtrich.Document.Blocks.Clear();
+                    txtrich.AppendText(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtrich.Document.Blocks.Clear();
+            listUser.Items.Clear();
+            txtUsername.Text = "";
+            txtPassword.Password = "";
+            txtAddress.Text = "";
+            txtMobile.Text = "";
+            txtTel.Text = "";
+            txtCountry.SelectedItem = "";
+            
+        }
+    }
+}
